8f2ac62591f30cdf06812f22f6eff2a38a876f48
DevilGuru <Shubhamverma6772@gmail.com>
2023-03-10 02:05:40 +0530

    initial-commit

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+
+import { ProductsProvider } from "./context/products_context";
+import { FilterProvider } from "./context/filter_context";
+import { CartProvider } from "./context/cart_context";
+import { UserProvider } from "./context/user_context";
+import { Auth0Provider } from "@auth0/auth0-react";
+
+//dev-8jwfbkhhv8554x0p.us.auth0.com
+//6wSOPs2mAumCIH1BTiOCaNo3oEwKrrEN
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+  <ProductsProvider>
+    <FilterProvider>
+      <CartProvider>
+        <App />
+      </CartProvider>
+    </FilterProvider>
+  </ProductsProvider>
+);
---
+++ src/utils/helpers.js
@@ -0,0 +1,17 @@
+export const formatPrice = (number ) => {
+ 
+    const newNumber = Intl.NumberFormat('en-IN',{
+         style:'currency',
+         currency:'INR'
+    }).format(((number/100)*81.86))
+
+    return newNumber
+}
+
+export const getUniqueValues = (data, type) => {
+    let unique = data.map((item)=>item[type])
+    if(type === 'colors'){
+        unique = unique.flat()
+    }
+    return ['all', ...new Set(unique)]
+}
